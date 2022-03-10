853a56d566280c0c1d4d9f2ec0ca4dccdc8bdc97
Abhinav Sharma <abhinav.161a026@gmail.com>
2022-03-10 17:07:49 +0530

    user following route

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -13,4 +13,59 @@ const getUserByEmail = async (req,res)=>{
     }
 }
 
-module.exports={getUserByEmail}
+const getFollowing= async(req,res)=>{
+    try{
+        const user= await User.find({email:req.user.email})
+        if(!user){
+            return res.status(400).json({msg:'User not found'})
+        }
+        return res.status(200).json(user)
+    }
+    catch(err){
+        return res.status(500).send(err.toString())
+    }
+}
+
+const removeFollowingInUser= async(req,res)=>{
+    try{
+        const user= await User.findOneAndUpdate({
+            email:req.user.email
+        },{
+            $pull:{
+                following:req.body.celeb_id
+            }
+        },{
+            returnOriginal:false
+        })
+        if(!user){
+            return res.status(400).json({msg:'User not found'})
+        }
+        return res.status(200).send('Removed celebrity from following')
+    }
+    catch(err){
+        return res.status(500).send(err.toString())
+    }
+}
+
+const addFollowingInUser= async(req,res)=>{
+    try{
+        const user= await User.findOneAndUpdate({
+            email:req.user.email
+        },{
+            $addToSet:{
+                following:req.body.celeb_id
+            }
+        },{
+            returnOriginal:false
+        })
+        if(!user){
+            return res.status(400).json({msg:'User not found'})
+        }
+        return res.status(200).send('Added celebrity to following')
+    }
+    catch(err){
+        return res.status(500).send(err.toString())
+    }
+}
+
+module.exports={getUserByEmail,addFollowingInUser,getFollowing,removeFollowingInUser}
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,9 +1,16 @@
 const express= require('express')
+const { addFollowingInUser, getFollowing, removeFollowingInUser } = require('../controllers/user.controller')
 const authenticated = require('../middleware/authenticated')
 const router= express.Router()
 
 router.get('/user/profile',authenticated,(req,res)=>{
     return res.status(200).json({profile_picture:req.user.profile_picture,username:req.user.username})
 })
+
+router.patch('/user/following',authenticated,addFollowingInUser)
+
+router.get('/user/following',authenticated,getFollowing)
+
+router.patch('/user/following/remove',authenticated,removeFollowingInUser)
 
 module.exports=router
