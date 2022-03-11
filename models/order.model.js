1745457a65ea452f2557bf66fb68e9df99019ad7
Pawan Sukhwani <sukhwanipawan@gmail.com>
2022-03-11 17:02:34 +0530

    orders Schema Created

---
+++ models/order.model.js
@@ -0,0 +1,16 @@
+const mongoose= require('mongoose')
+
+const orderSchema = new mangoose.Schema({
+    fore:{type:String,required:true},
+    from:{type:String,required:true},
+    occassion:{type:String,required:true},
+    instructions:{type:String,required:true},
+    optional:{type:String,required:false},
+    price:{type:Number,required:true},
+    celeb_id:{type:String,required:true},
+    user_id:{type:String,required:true},
+})
+
+const Orders = mongoose.model('orders',orderSchema)
+
+module.exports= Orders
--- models/users.model.js
+++ models/users.model.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const bcrypt= require('bcrypt')
+
 const UserSchema = new mongoose.Schema({
     email:{type: String, required:true, unique:true},
     password:{type: String},
