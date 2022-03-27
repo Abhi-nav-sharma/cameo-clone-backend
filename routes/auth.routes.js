07c5b05357cf405840b696f7a01856b8ab328f45
Abhinav Sharma <abhinav.161a026@gmail.com>
2022-03-27 13:36:25 +0530

    fix: uncommented passport

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -6,30 +6,30 @@ const url = require('url');
 const validateSignin = require('../utils/validateSignin');
 const validateSignup = require('../utils/validateSignup');
 
-// router.get('/auth/logout',(req,res)=>{
-//     req.logout()
-//     res.redirect('http://localhost:3000/')
-// })
+router.get('/auth/logout',(req,res)=>{
+    req.logout()
+    res.redirect('http://localhost:3000/')
+})
 
 router.post('/auth/signin',...validateSignin(),signin)
 
 router.post("/auth/signup",...validateSignup(),signup)
 
-// router.get('/auth/facebook',
-//   passport.authenticate('facebook',{ scope : ['email'] }));
+router.get('/auth/facebook',
+  passport.authenticate('facebook',{ scope : ['email'] }));
 
-// router.get('/auth/facebook/callback',
-//   passport.authenticate('facebook', { failureRedirect: '/login' }),
-//   function(req, res) {
-//     const token= generateToken(req.user)
-//     // res.cookie('token',token)
-//     res.redirect(url.format({
-//         pathname:'http://localhost:3000/',
-//         query:{
-//             "token":token,
-//             "user":req.user.email
-//         }
-//     }))
-//   });
+router.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+    const token= generateToken(req.user)
+    // res.cookie('token',token)
+    res.redirect(url.format({
+        pathname:'http://localhost:3000/',
+        query:{
+            "token":token,
+            "user":req.user.email
+        }
+    }))
+  });
 
   module.exports=router
